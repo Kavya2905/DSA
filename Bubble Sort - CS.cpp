fdd18ac2451f80f5d8658a97811ebbb9a7b4aee3
Kavya2905 <74311514+Kavya2905@users.noreply.github.com>
2022-05-12 08:20:49 +0530

    Update Bubble Sort - CS.cpp

--- Bubble Sort - CS.cpp
+++ Bubble Sort - CS.cpp
@@ -2,12 +2,15 @@ void bubbleSort(vector<int>& arr, int n)
 {   
 	for(int i = 1; i < n; i++){
 		//Rounds = 1 to n-1
+		bool swapped = false;
 		for(int j = 0; j < n-i; j++){
 			//Process adjacent elements
 			if(arr[j] > arr[j + 1]){
+				swapped = true;
 				swap(arr[j + 1], arr[j]);
 			}
 		}
+		swapped = false;
 	}
-    // Write your code here.
+    
 }
